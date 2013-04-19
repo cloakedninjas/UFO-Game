4b58a3df83d05615469b3b0176651219116cb45e
Daniel Jackson <danyboyj@gmail.com>
2013-04-19 22:26:16 +0100

    Initial commit

---
+++ public_html/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>UFO game</title>
+        <script type="text/javascript" src="/js/jquery-1.9.1.min.js"></script>
+        <script type="text/javascript" src="/js/easeljs-0.6.0.min.js"></script>
+        <script type="text/javascript" src="/js/ufo.js"></script>
+        
+        <link rel="stylesheet" href="/css/main.css" />
+        
+    </head>
+    
+    <body>
+    
+        <canvas id="game" width="500" height="500"></canvas>
+        
+    </body>
+    
+    
+
+</html>
---
+++ public_html/js/ufo.js
@@ -0,0 +1,88 @@
+'usestrict';
+
+var Game = {
+
+    viewport: {
+        width: 0,
+        height: 0
+    },
+    $canvas: null,
+    stage: null,
+
+    bg: [],
+    ufo: null,
+    entities: [],
+
+    init: function() {
+
+        this.viewport.width = $(document).width();
+        this.viewport.height = 438; //$(document).height();
+
+        this.$canvas = $('#game');
+        this.$canvas.attr({
+            width: this.viewport.width,
+            height: this.viewport.height
+        });
+
+        this.stage = new createjs.Stage(this.$canvas[0]);
+
+        // preload assets
+
+        // build BG
+        this.buildBg();
+
+        // create UFO
+
+        // start
+
+        var game = this;
+        createjs.Ticker.addEventListener("tick", function(event, target) {
+            game.tick(event, target, game);
+        });
+
+        
+    },
+
+    buildBg: function() {
+
+        var cloud = new createjs.Bitmap("/images/cloud.png");
+        cloud.set({x: 40, y: 10});
+
+        this.bg.push(cloud);
+
+        this.stage.addChild(cloud);
+    },
+
+    tick: function(event, target, game) {
+        if (event.paused) {
+            return;
+        }
+
+        // move clouds
+        for (var i = 0; i < this.bg.length; i++) {
+            var newX = this.bg[i].x - 5;
+
+            if (newX.x + this.bg[i].width < 0) {
+                delete this.bg[i];
+                continue;
+            }
+            game.bg[i].set({x: newX});
+
+            //console.log(game.bg[i].visible);
+        }
+        this.stage.update();
+    }
+};
+
+var Cloud = function() {
+    this.x = 0;
+    this.y = 0;
+
+    this.tick = function() {
+        this.x += 5;
+    }
+};
+
+$(document).ready(function() {
+   Game.init();
+});
