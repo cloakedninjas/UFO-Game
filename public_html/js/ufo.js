1255243130605d0451d3417891215871f7f4440e
Daniel Jackson <danyboyj@gmail.com>
2013-04-21 23:06:24 +0100

    More parallaxing

--- public_html/js/ufo.js
+++ public_html/js/ufo.js
@@ -11,7 +11,14 @@ var Game = {
     $canvas: null,
     stage: null,
 
-    bg: [],
+    clouds: [],
+	grass: {
+		shape: null,
+		matrix: null,
+		img: null,
+		x: 0,
+		z: 10
+	},
     ufo: null,
     cows: [],
 
@@ -29,7 +36,7 @@ var Game = {
 			images: ['/images/cow_grass.png'],
 			frames: {width:46, height:34},
 			animations: {
-				munch: [0,3]
+				munch: [0,3, 'munch', 5]
 			}
 		}
 	},
@@ -96,23 +103,27 @@ var Game = {
 
     buildBg: function() {
 
-		for (var i = 0; i < 2; i++) {
-        	var cloud = new createjs.Bitmap("/images/cloud.png");
+		var cloud = new createjs.Bitmap("/images/cloud.png");
 
-			if (typeof x !== 'undefined') {
-				x += Math.round(Math.random() * this.viewport.width);
-			}
+		var x = Math.round(Math.random() * this.viewport.width);
 
-			var x = Math.round(Math.random() * this.viewport.width);
+		cloud.set({x: x, y: 10, z: 10});
 
-        	cloud.set({x: x, y: 10, z: 10});
+		this.clouds.push(cloud);
+		this.stage.addChild(cloud);
 
-        	this.bg.push(cloud);
 
-        	this.stage.addChild(cloud);
-		}
+		this.grass.img = new Image();
+		this.grass.img.src = '/images/grass_parallax.png';
 
-    },
+		this.grass.matrix = new createjs.Matrix2D();
+		this.grass.matrix.translate(0, -3);
+
+		var g = new createjs.Graphics().beginBitmapFill(this.grass.img, 'repeat-x', this.grass.matrix);
+		g.drawRect(0,291, this.viewport.width,14);
+		this.grass.shape = new createjs.Shape(g);
+		this.stage.addChild(this.grass.shape);
+	},
 
     tick: function(event, target, game) {
         if (event.paused) {
@@ -132,12 +143,10 @@ var Game = {
 			this.beam();
 		}
 
-
-
         // move clouds
-        for (var i = 0; i < this.bg.length; i++) {
+        for (var i = 0; i < this.clouds.length; i++) {
 
-			var elem = game.bg[i];
+			var elem = this.clouds[i];
 
             var newX = this._getScrolledX(elem);
 
@@ -146,8 +155,19 @@ var Game = {
             }
 
             elem.set({x: newX});
-
         }
+
+		// move grass
+		newX = this._getScrolledX(this.grass);
+		this.grass.x = newX;
+
+		//console.log(newX);
+		this.grass.matrix.translate(newX, -3);
+
+		this.grass.shape.graphics.clear().beginBitmapFill(this.grass.img, 'repeat-x', this.grass.matrix).drawRect(0,291, this.viewport.width,14);
+
+		//this.stage.addChild(this.grass.shape);
+
 
 		// ensure we have a steady supply of cows
 		var lastCow = this.cows[this.cows.length-1];
@@ -172,16 +192,17 @@ var Game = {
     },
 
 	_addCow: function(immediate) {
-		var muncher = (Math.random() >= 0.7);
+		var muncher = (Math.random() >= 0.6);
+
+		var cow;
 
 		if (muncher) {
-			console.log('muncher');
             var spritesheet = new createjs.SpriteSheet(this.spritesheets.cow);
-			var cow = new createjs.BitmapAnimation(spritesheet);
+			cow = new createjs.BitmapAnimation(spritesheet);
 			cow.gotoAndPlay('munch');
 		}
 		else {
-			var cow = new createjs.Bitmap("/images/cow.png");
+			cow = new createjs.Bitmap("/images/cow.png");
 		}
 
 		var y = 300 + Math.round(Math.random() * 100);
